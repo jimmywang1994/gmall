5fc7c8d364ae2ad3e4d76e47fa3c453cdc9ec038
wwei <429744781@qq.com>
2020-01-09 16:58:00 +0800

    加入springcloud（为了方便切换，dubbo没有删除）

--- gmall-user/src/main/java/com/ww/gmall/user/GmallUserApplication.java
+++ gmall-user/src/main/java/com/ww/gmall/user/GmallUserApplication.java
@@ -4,8 +4,9 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDubbo
+@EnableEurekaClient
 @MapperScan("com.ww.gmall.user.mapper")
 @SpringBootApplication
 public class GmallUserApplication {
--- gmall-user/src/test/java/com/ww/gmall/user/GmallUserApplicationTests.java
+++ gmall-user/src/test/java/com/ww/gmall/user/GmallUserApplicationTests.java
@@ -1,13 +1,9 @@
 package com.ww.gmall.user;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallUserApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
