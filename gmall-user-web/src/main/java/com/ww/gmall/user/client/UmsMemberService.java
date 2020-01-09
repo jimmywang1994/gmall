2182dc37a69bed5c2a7d2e95cf28370c2f7ae5f1
Wang wei <429744781@qq.com>
2020-01-09 20:26:28 +0800

    springcloud改造完成，增加springboot admin健康监控模块

--- gmall-user-web/src/main/java/com/ww/gmall/user/client/UmsMemberService.java
+++ gmall-user-web/src/main/java/com/ww/gmall/user/client/UmsMemberService.java
@@ -3,12 +3,13 @@ package com.ww.gmall.user.client;
 import com.ww.gmall.bean.UmsMember;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.awt.*;
 import java.util.List;
 
 @FeignClient(value = "gmall-user")
 public interface UmsMemberService {
-    @GetMapping("/user/ums-member/allMembers")
+    @RequestMapping("/user/ums-member/allMembers")
     public List<UmsMember> allMembers();
 }
