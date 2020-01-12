f3295f4dd11b509cede45ffe7acf8905ea73bf35
Wang wei <429744781@qq.com>
2020-01-12 23:02:39 +0800

    查詢平台屬性

--- gmall-admin/src/main/java/com/ww/gmall/pms/controller/BaseAttrValueController.java
+++ gmall-admin/src/main/java/com/ww/gmall/pms/controller/BaseAttrValueController.java
@@ -1,8 +1,15 @@
 package com.ww.gmall.pms.controller;
 
 
+import com.ww.gmall.pms.bean.BaseAttrValue;
+import com.ww.gmall.pms.client.BaseAttrService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,7 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-01-12
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/pms/base-attr-value")
 public class BaseAttrValueController {
+    @Autowired
+    BaseAttrService baseAttrService;
 
+    @RequestMapping("getAttrValueList")
+    public List<BaseAttrValue> getAttrValueList(@RequestParam("attrId") String attrId) {
+        return baseAttrService.getAttrValueList(attrId);
+    }
 }
