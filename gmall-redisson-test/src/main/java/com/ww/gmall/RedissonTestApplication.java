df8cd05703d4a28d461eceb8ca01dcbd66263bf0
Jimmy <429744781@qq.com>
2020-01-20 22:34:11 +0800

    增加redisson分布式锁框架模块并配置

---
+++ gmall-redisson-test/src/main/java/com/ww/gmall/RedissonTestApplication.java
@@ -0,0 +1,10 @@
+package com.ww.gmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class RedissonTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonTestApplication.class,args);
+    }
+}
